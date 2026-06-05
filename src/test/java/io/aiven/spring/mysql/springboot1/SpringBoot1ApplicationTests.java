31b1059e7a970c78e3950956c16fae03e3d1adab
Ganesh Kulakarni <ganeshkulakarni124@gmail.com>
2026-06-05 23:31:11 +0530

    Initial commit with clean history

---
+++ src/test/java/io/aiven/spring/mysql/springboot1/SpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aiven.spring.mysql.springboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
